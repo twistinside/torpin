308bc9792f11d564a015b4ab0e698d9e4791918c
twistinside <twistinside@users.noreply.github.com>
2024-10-09 13:41:32 -0700

    Remove base path mapping

--- cdk/lib/torpin-stack.ts
+++ cdk/lib/torpin-stack.ts
@@ -99,9 +99,6 @@ export class TorpinStack extends Stack {
       endpointType: EndpointType.REGIONAL,  // Ensure it's Regional
     });
     
-    // Map custom domain to the API Gateway stage
-    customDomain.addBasePathMapping(api, { basePath: '' });
-    
     // Create an A Record in Route 53 for the custom domain
     new ARecord(this, 'ApiARecord', {
       zone: hostedZone,
