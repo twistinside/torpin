d6a80180e035305a61cc87296ebbd140da4ec04e
twistinside <twistinside@users.noreply.github.com>
2024-10-09 13:34:23 -0700

    Fix base path

--- cdk/lib/torpin-stack.ts
+++ cdk/lib/torpin-stack.ts
@@ -100,7 +100,7 @@ export class TorpinStack extends Stack {
     });
     
     // Map custom domain to the API Gateway stage
-    customDomain.addBasePathMapping(api, { basePath: 'v1' });
+    customDomain.addBasePathMapping(api, { basePath: 'prod' });
     
     // Create an A Record in Route 53 for the custom domain
     new ARecord(this, 'ApiARecord', {
